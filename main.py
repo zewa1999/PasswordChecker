5b769c52ff710d8bb7f138009d8564d874932fa8
Andrei Costache <costacheandrei43@gmail.com>
2021-09-07 15:05:27 +0300

    Added project

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+import requests
+import hashlib
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again!')
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
+def pwned_api_check(password):
+    sha1_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1_password[:5], sha1_password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+
+def main():
+    print('Fill in your password:\n')
+    password = str(input())
+    count = pwned_api_check(password)
+    if count:
+        print(f'{password} was found {count} times... You should change your password')
+    else:
+        print(f'{password} was NOT found. You are safe!')
+    return 'done!'
+
+
+if __name__ == '__main__':
+    sys.exit(main())
